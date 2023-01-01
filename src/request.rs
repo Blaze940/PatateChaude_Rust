cd2efb6fae412d726c220f65349daba9b802af61
Pierre <bonnamy.77@gmail.com>
2023-01-01 19:50:45 +0100

    typo

--- src/request.rs
+++ src/request.rs
@@ -3,11 +3,11 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 
 pub fn send (stream: &mut TcpStream, msg: Message) {
-    send_to_server(stream, msg);
-    receive_from_server(stream);
+    send_input(stream, msg);
+    receive_output(stream);
 }
 
-fn send_to_server (stream: &mut TcpStream, msg: Message) {
+fn send_input (stream: &mut TcpStream, msg: Message) {
     let json = serde_json::to_string(&msg).unwrap();
     println!("Sended: {:?}", json);
     let len = json.len() as u32;
@@ -16,7 +16,7 @@ fn send_to_server (stream: &mut TcpStream, msg: Message) {
     stream.write(&json.as_bytes()).unwrap();
 }
 
-fn receive_from_server (stream: &mut TcpStream) -> String {
+fn receive_output (stream: &mut TcpStream) -> String {
     let mut buf_len = [0u8; 4];
     stream.read_exact(buf_len.as_mut()).unwrap();
     let len = u32::from_be_bytes(buf_len);
