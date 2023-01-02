2e6e697767e6eb252dc0138a2c44325ae0304b06
Emmanuel <manuboisset@gmail.com>
2023-01-02 23:53:53 +0100

    init trait initComplexity

--- src/hashCash.rs
+++ src/hashCash.rs
@@ -1,4 +1,18 @@
 mod hashCashStruct;
+use crate::structs::MD5HashCashInput;
+
+trait getComplexity {
+
+    fn initcomplexity() -> unit;
+}
+
+impl getComplexity for MD5HashCashInput {
+    fn initcomplexity() -> unit {
+        todo!()
+    }
+
+}
+
 
 
 fn get_input() -> bool{
